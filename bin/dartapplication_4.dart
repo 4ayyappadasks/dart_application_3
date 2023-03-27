9f977c3def4976d246b83c9095144ce23c9342a0
4ayyappadasks <ayyappanks141@gmail.com>
2023-03-27 22:10:47 +0530

    commit

---
+++ bin/dartapplication_4.dart
@@ -0,0 +1,14 @@
+void main() {
+  String heading = "\t DETAILS OF STUDENTS";
+  String name = "ayyappadas";
+  String address =
+      "kappadathottiyil \n \t \t enanaloor po \n \t \t anchelpetty";
+  String cource = "flutter";
+  int period = 4;
+
+  print(heading);
+  print('name           :$name');
+  print("address        :$address");
+  print("cource selected:$cource");
+  print("period         :$period");
+}
