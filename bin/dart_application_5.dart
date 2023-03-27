75f39d5b40720e636281985d6f078449683cd1cb
4ayyappadasks <ayyappanks141@gmail.com>
2023-03-27 23:23:30 +0530

    commit

---
+++ bin/dart_application_5.dart
@@ -0,0 +1,18 @@
+void main() {
+  String name = "eee";
+  int totalstudents = 23;
+  int total_boys = 10;
+  int total_girls = 13;
+  int total_present = 20;
+  int total_boys_present = 10;
+  int total_girls_present = 10;
+
+  print("\t branch wice attentence");
+  print("name of branch       : $name");
+  print("total no of students : $totalstudents");
+  print("total no of boys     : $total_boys");
+  print("total no of girls    : $total_girls");
+  print("total boys present   : $total_boys_present");
+  print("total girls present  : $total_girls_present");
+  print("total present        : $total_present");
+}
