eadcbe89536c6384109203d9384d4af0eee1f94c
4ayyappadasks <ayyappanks141@gmail.com>
2023-03-27 10:22:30 +0530

    first commit

---
+++ bin/dart_application_3.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  // variable variable_name ="name",num
+String name = "ayyappadas";
+int age = 23;
+int phone = 9446422041;
+double mark = 50.2;
+String email = "ayyappanks141@gamail.com";
+
+//ptint as output
+
+print("my name is : $name");
+print("am $age year old");
+print("my mobile no is $phone");
+print("mark obtained: $mark");
+print("mail : $email");
+}
