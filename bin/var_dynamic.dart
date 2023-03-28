d6086f73d076ef430ef6abbd9dae6051b0ef2e80
4ayyappadasks <ayyappanks141@gmail.com>
2023-03-28 11:32:09 +0530

    commit

---
+++ bin/var_dynamic.dart
@@ -0,0 +1,9 @@
+void main() {
+  var a = 22; // var il variable automatic select aakum but pinne mattan pattula
+  dynamic b =
+      "name"; // dynamic il variable automatic select aakum ,athupole pinnidu change aakum
+  print(a);
+  print(b);
+  b = 10;
+  print(b);
+}
